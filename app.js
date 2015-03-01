dd17d7c1b6ee19a949959b502d5341b0cdd4cd7a
scotchfield <swrittenb@gmail.com>
2015-02-28 20:59:53 -0500

    Better JS structure. Add info to package, add public static dir.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+'use strict';
+
 var express = require('express'),
     request = require('request'),
-    mongoose = require('mongoose');
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
 
-var url = 'http://api.reddit.com/hot.json',
-    headers = {'User-Agent': 'Buster/1.0.0 (by /u/scotchfield)'};
+    url = 'http://api.reddit.com/hot.json',
+    headers = {'User-Agent': 'Buster/1.0.0 (by /u/scotchfield)'},
 
-var app = express(), port = 8000;
+    app = express(), port = 8000;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost/buster');
 
@@ -13,38 +21,34 @@ var Link = new mongoose.Schema({
   name: String,
   url: String,
   date: { type: Date, default: Date.now }
-});
-var LinkModel = mongoose.model('Link', Link);
+}),
+LinkModel = mongoose.model('Link', Link),
 
-var getDataReddit = function (body) {
+getDataReddit = function (body) {
   var data = [];
-  body.data.children.forEach(
-    function (result) {
-      console.log(result);
-      data.push({name: result.data.title, url: result.data.url});
-    }
-  );
+  body.data.children.forEach(function (result) {
+    console.log(result);
+    data.push({name: result.data.title, url: result.data.url});
+  });
   return data;
-}
+},
 
-var doJsonRequest = function (url, headers, dataFunction) {
+doJsonRequest = function (url, headers, dataFunction) {
   request({url: url, headers: headers, json: true},
           function (error, response, body) {
     if (! error && response.statusCode === 200) {
-      dataFunction(body).forEach(
-        function (data) {
-          LinkModel.find({name: data.name, url: data.url},
-                         function (err, docs) {
-            if (docs.length === 0) {
-              var link = new LinkModel({name: data.name, url: data.url});
-              link.save(function (err) {});
-              console.log('* Added: ' + data.name);
-            } else {
-              console.log('Existing: ' + data.name);
-            }
-          });
-        }
-      )
+      dataFunction(body).forEach(function (data) {
+        LinkModel.find({name: data.name, url: data.url},
+                       function (err, docs) {
+          if (docs.length === 0) {
+            var link = new LinkModel({name: data.name, url: data.url});
+            link.save(function (err) {});
+            console.log('* Added: ' + data.name);
+          } else {
+            console.log('Existing: ' + data.name);
+          }
+        });
+      });
     }
   });
 };
@@ -52,9 +56,9 @@ var doJsonRequest = function (url, headers, dataFunction) {
 //doJsonRequest(url, headers, getDataReddit);
 
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('hello world');
-});
+});*/
 
 app.get('/links', function(req, res) {
   LinkModel.find({}, function (err, docs) {
