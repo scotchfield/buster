38ccc474aaf8b3ae24f85b3c5ff3ff30b4c3db0b
scotchfield <swrittenb@gmail.com>
2015-02-28 22:05:37 -0500

    Display name/url info from json pulled from db

---
+++ public/js/buster.js
@@ -0,0 +1,60 @@
+var BusterBox = React.createClass({
+  loadFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType:'json',
+      success: function(data) {
+        this.setState({data: data});
+        console.log(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadFromServer();
+  },
+  render: function() {
+    return (
+      <div className="busterBox">
+        <h1>Available Tweets</h1>
+        <BusterList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var BusterList = React.createClass({
+  render: function() {
+    var busterNodes = this.props.data.map(function (node) {
+      return (
+        <BusterNode name={node.name} url={node.url} />
+      );
+    });
+    return (
+      <div className="busterList">
+        {busterNodes}
+      </div>
+    );
+  }
+});
+
+var BusterNode = React.createClass({
+  render: function() {
+    return (
+      <div className="busterNode">
+        <span>{this.props.name} {this.props.url}</span>
+      </div>
+    );
+  }
+});
+
+
+React.render(
+  <BusterBox url="/links" />,
+  document.getElementById('content')
+);
